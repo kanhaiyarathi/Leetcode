48a39678e13934447e8f364eef620e153b90ebae
kanhaiyarathi <kanhaiya.rathi07@gmail.com>
2020-04-15 13:17:00 +0530

    Medium
    https://leetcode.com/problems/product-of-array-except-self/

---
+++ 238-product-of-array-except-self.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();        
+        vector<int> ans(n,1);
+        
+        if(n==0)
+            return ans;
+        
+       int currProd = 1;
+        
+        for(int i=n-1;i>=0;i--){
+            ans[i] *= currProd;
+            currProd*=nums[i];
+        }
+        
+       currProd = 1;
+        
+        for(int i=0;i<n;i++){
+            ans[i]=ans[i]*currProd;           
+            currProd*=nums[i];
+        }
+        for(int i=0;i<n;i++)
+            cout << ans[i] << "  ";
+        return ans;
+    }
+};
